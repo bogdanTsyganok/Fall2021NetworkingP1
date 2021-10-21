2cd1fc4a68ff8ca03b77fee6a0a0d7e1f8d40bf6
Jarrid Steeper <www.keatine@hotmail.com>
2021-10-21 17:10:31 -0400

    Fixed buffer bit shifting

--- StaticBufferLibrary/cBuffer.cpp
+++ StaticBufferLibrary/cBuffer.cpp
@@ -25,7 +25,7 @@ void cBuffer::WriteIntBE(int32_t value)
 
 int32_t cBuffer::ReadIntBE(std::size_t index)
 {
-	int value = 0;
+	uint32_t value = 0;
 	value |= mBuffer[index] << 24;
 	value |= mBuffer[index + 1] << 16;
 	value |= mBuffer[index + 2] << 8;
@@ -47,39 +47,31 @@ int32_t cBuffer::ReadIntBE()
 //Floats
 void cBuffer::WriteShortBE(std::size_t index, int16_t value)
 {
-	mBuffer[index] = value >> 12;
-	mBuffer[index + 1] = value >> 8;
-	mBuffer[index + 2] = value >> 4;
-	mBuffer[index + 3] = value;
+	mBuffer[index] = value >> 8;
+	mBuffer[index + 1] = value;
 }
 
 void cBuffer::WriteShortBE(int16_t value)
 {
-	mBuffer[mWriteIndex] = value >> 12;
-	mBuffer[mWriteIndex + 1] = value >> 8;
-	mBuffer[mWriteIndex + 2] = value >> 4;
-	mBuffer[mWriteIndex + 3] = value;
-	mWriteIndex += 4;
+	mBuffer[mWriteIndex] = value >> 8;
+	mBuffer[mWriteIndex + 1] = value;
+	mWriteIndex += 2;
 }
 
 int16_t cBuffer::ReadShortBE(std::size_t index)
 {
-	int value = 0;
-	value |= mBuffer[index] << 12;
-	value |= mBuffer[index + 1] << 8;
-	value |= mBuffer[index + 2] << 4;
-	value |= mBuffer[index + 3];
+	int16_t value = 0;
+	value |= mBuffer[index] << 8;
+	value |= mBuffer[index + 1];
 	return value;
 }
 
 int16_t cBuffer::ReadShortBE()
 {
-	uint32_t value = 0;
-	value |= mBuffer[mReadIndex] << 12;
-	value |= mBuffer[mReadIndex + 1] << 8;
-	value |= mBuffer[mReadIndex + 2] << 4;
-	value |= mBuffer[mReadIndex + 3];
-	mReadIndex += 4;
+	int16_t value = 0;
+	value |= mBuffer[mReadIndex] << 8;
+	value |= mBuffer[mReadIndex + 1];
+	mReadIndex += 2;
 	return value;
 }
 
@@ -88,10 +80,8 @@ void cBuffer::WriteStringBE(std::size_t index, std::string value)
 {
 	for (char c : value)
 	{
-		mBuffer[index] = c >> 6;
-		mBuffer[index + 1] = c >> 4;
-		mBuffer[index + 2] = c >> 2;
-		mBuffer[index + 3] = c;
+		mBuffer[index] = c;
+		index++;
 	}
 }
 
@@ -99,11 +89,8 @@ void cBuffer::WriteStringBE(std::string value)
 {
 	for (char c : value)
 	{
-		mBuffer[mWriteIndex] = c >> 6;
-		mBuffer[mWriteIndex + 1] = c >> 4;
-		mBuffer[mWriteIndex + 2] = c >> 2;
-		mBuffer[mWriteIndex + 3] = c;
-		mWriteIndex += 4;
+		mBuffer[mWriteIndex] = c;
+		mWriteIndex++;
 	}
 }
 
@@ -113,10 +100,7 @@ std::string cBuffer::ReadStringBE(std::size_t index, int32_t stringSize)
 	std::string outPut;
 	for (int i = 0; i < stringSize; i++)
 	{
-		value |= mBuffer[index] << 6;
-		value |= mBuffer[index + 1] << 4;
-		value |= mBuffer[index + 2] << 2;
-		value |= mBuffer[index + 3];
+		value |= mBuffer[index++];
 		outPut += value;
 	}
 
@@ -129,10 +113,7 @@ std::string cBuffer::ReadStringBE(int32_t stringSize)
 	std::string outPut;
 	for (int i = 0; i < stringSize; i++)
 	{
-		value |= mBuffer[mReadIndex] << 6;
-		value |= mBuffer[mReadIndex + 1] << 4;
-		value |= mBuffer[mReadIndex + 2] << 2;
-		value |= mBuffer[mReadIndex + 3];
+		value |= mBuffer[mReadIndex++];
 		outPut += value;
 	}
 
